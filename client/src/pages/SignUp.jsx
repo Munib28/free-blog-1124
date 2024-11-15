501d730c9a8894415203b80c910ec62a7e421803
Munib <munib.obradovic@gmail.com>
2024-11-15 18:49:25 +0100

    Complete Sign Up Page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { Button, Label, TextInput } from "flowbite-react";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* LEFT SIDE */}
+        <div className="flex-1">
+          <Link to="/">
+            <img className="w-48" src={logo} alt="" />
+          </Link>
+          <p className="text-sm font mt-5">
+            This is a Free Blog Application. You can sign up with your email and
+            password or with Google.
+          </p>
+        </div>
+        {/* RIGHT SIDE */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your Username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput type="text" placeholder="Email" id="email" />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button type="submit">Sign Up</Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-4">
+            <span>Have an account? </span>
+            <Link to="/sign-in" className="text-orange-600 font-semibold">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
